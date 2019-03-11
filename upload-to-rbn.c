03ca66d93faa6d4b49a86d3f39ca40f34da8a3f2
Bjorn Ekelund <bjorn@ekelund.nu>
2019-03-11 17:11:39 +0100

    Removed unnecessary size calculations

--- upload-to-rbn.c
+++ upload-to-rbn.c
@@ -66,9 +66,9 @@ int main(int argc, char *argv[]) {
   char *src, *dst, *start;
 
   // Header including schema
-  char header[] = { 0xAD, 0xBC, 0xCB, 0xDA, 0x00, 0x00, 0x00, 0x02 };
-  char msg1[] = { 0x00, 0x00, 0x00, 0x01 };  // Message number for status datagram
-  char msg2[] = { 0x00, 0x00, 0x00, 0x02 }; // Message number for decode datagram
+  char header[8] = { 0xAD, 0xBC, 0xCB, 0xDA, 0x00, 0x00, 0x00, 0x02 };
+  char msg1[4] = { 0x00, 0x00, 0x00, 0x01 };  // Message number for status datagram
+  char msg2[4] = { 0x00, 0x00, 0x00, 0x02 }; // Message number for decode datagram
 
   unsigned short broadcastPort; // IP broadcast port
   char *broadcastIP; // IP broadcast address
@@ -151,36 +151,30 @@ int main(int argc, char *argv[]) {
       /*************************************************/
 
       memcpy(buffer, header, sizeof(header)); // Start with header
-      size = sizeof(header);
       dst = buffer + sizeof(header);
-
-      memcpy(dst, msg1, sizeof(msg1)); // Message ID
-      size += sizeof(msg1);
+      memcpy(dst, msg1, sizeof(msg1)); // Message identifier
       dst += sizeof(msg1);
-
-      copy_char(&dst, ID); size += strlen(ID) + 4; // Receiver software ID - ignored by RBNA
-
+      copy_char(&dst, ID); // Receiver software ID - ignored by RBNA
       copy_int4(&dst, 0);       // Base frequency as 8 byte integer
       copy_int4(&dst, bfreq);
-      size += 8;
+      copy_char(&dst, "FT8");    // Rx Mode - ignored by RBNA
+      copy_char(&dst, call);     // DX call - ignored by RBNA
+      copy_char(&dst, ssnr);     // SNR as string - ignored by RBNA
+      copy_char(&dst, "FT8");    // Tx Mode - ignored by RBNA
+      copy_int1(&dst, 0);        // TX enable = false - ignored by RBNA
+      copy_int1(&dst, 0);        // Transmitting = false - ignorded by RBNA
+      copy_int1(&dst, 0);        // Decoding = false - ignored by RBNA
+      copy_int4(&dst, 0);        // rxdf - ignored by RBNA
+      copy_int4(&dst, 0);        // txdf - ignored by  RBNA
+      copy_char(&dst, "AB1CDE"); // DE call - ignored by RBNA
+      copy_char(&dst, "AB12");   // DE grid - ignored by RBNA
+      copy_int1(&dst, 0);        // TX watchdog = false - ignored by RBNA
+      copy_char(&dst, "");       // Submode - ignored by RBNA
+      copy_int1(&dst, 0);        // Fast mode = false - ignored by RBNA
+      copy_int1(&dst, 0);        // Special operation mode = 0 - ignored by RBNA
 
-      copy_char(&dst, "FT8"); size += strlen("FT8") + 4;  // Rx Mode - ignored by RBNA
-      copy_char(&dst, call); size += strlen(call) + 4; // DX call - ignored by RBNA
-      copy_char(&dst, ssnr); size += strlen(ssnr) + 4; // SNR as string - ignored by RBNA
-      copy_char(&dst, "FT8"); size += strlen("FT8") + 4; // Tx Mode - ignored by RBNA
-      copy_int1(&dst, 0); size += 1; // TX enable = false - ignored by RBNA
-      copy_int1(&dst, 0); size += 1; // Transmitting = false - ignorded by RBNA
-      copy_int1(&dst, 0); size += 1; // Decoding = false - ignored by RBNA
-      copy_int4(&dst, 0); size += 4; // rxdf - ignored by RBNA
-      copy_int4(&dst, 0); size += 4; // txdf - ignored by  RBNA
-      copy_char(&dst, "AB1CDE"); size += strlen("AB1CDE") + 4; // DE call - ignored by RBNA
-      copy_char(&dst, "AB12"); size += strlen("AB12") + 4; // DE grid - ignored by RBNA
-      copy_int1(&dst, 0); size += 1; // TX watchdog = false - ignored by RBNA
-      copy_char(&dst, ""); size += strlen("") + 4; // Submode - ignored by RBNA
-      copy_int1(&dst, 0); size += 1; // Fast mode = false - ignored by RBNA
-      copy_int1(&dst, 0); size += 1; // Special operation mode = 0 - ignored by RBNA
-
-//      printf("Size: %3d dst-buffer: %3d ", size, (int)(dst-buffer));
+      size = dst - buffer;
+//      printf("Status: size: %3d ", size);
 //      printf("Message:\n"); for (i = 0; i < size; i++) printf("%02X ", buffer[i] & 0xFF); printf("\n");
 
       if (sendto(sock, buffer, size, 0, (struct sockaddr *)&broadcastAddr,
@@ -194,28 +188,27 @@ int main(int argc, char *argv[]) {
       /*************************************************/
 
       memcpy(buffer, header, sizeof(header)); // Header including schema information
-      size = sizeof(header);
       dst = buffer + sizeof(header);
-
       memcpy(dst, msg2, sizeof(msg2)); // Message identifier
-      size += sizeof(msg2);
       dst += sizeof(msg2);
+      copy_char(&dst, ID);      // Software ID - ignored by RBNA
+      copy_int1(&dst, 1);       // New decode = true
+      copy_int4(&dst, 0);       //Time = zero - ignored by RBNA
+      copy_int4(&dst, snr);  	// Report as 4 byte integer
+      copy_double(&dst, dt); 	// Delta time - ignored by RBNA
+      copy_int4(&dst, hz);      // Delta frequency in hertz - ignored by RBNA
+      copy_char(&dst, "FT8");   // Receive mode - ignored by RBNA
+      copy_char(&dst, message); // Fake message based on decode
+      copy_int1(&dst, 0);       // Low confidence = false - ignored by RBNA
+      copy_int1(&dst, 0);       // Off air = false - ignored by RBNA
 
-      copy_char(&dst, ID); size += strlen(ID) + 4; // Software ID
-      copy_int1(&dst, 1); size += 1; // New = true
-      copy_int4(&dst, 0); size += 4; //Time = zero - ignored by RBNA
-      copy_int4(&dst, snr); size += 4; // Report as 4 byte integer
-      copy_double(&dst, (double)dt); size += 8; // Delta time - ignored by RBNA
-      copy_int4(&dst, hz); size += 4; // Delta frequency in hertz - ignored by RBNA
-      copy_char(&dst, "FT8"); size += strlen("FT8") + 4; // Receive mode - ignored by RBNA
-      copy_char(&dst, message); size += strlen(message) + 4; // Fake message based on decode
-      copy_int1(&dst, 0); size += 1; // Low confidence = false - ignored by RBNA
-      copy_int1(&dst, 0); size += 1; // Off air = false - ignored by RBNA
+      size = dst - buffer;
 
-//      printf("Size: %3d dst-buffer: %3d ", size, (int)(dst-buffer));
+//      printf("Decode: size: %3d\n", size);
 //      printf("Message:\n"); for (i = 0; i < size; i++) printf("%02X ", buffer[i] & 0xFF); printf("\n");
 
-      if (sendto(sock, buffer, size, 0, (struct sockaddr *)&broadcastAddr, sizeof(broadcastAddr)) != size) {
+      if (sendto(sock, buffer, size, 0, (struct sockaddr *)&broadcastAddr, 
+          sizeof(broadcastAddr)) != size) {
         fprintf(stderr, "sendto() sent a different number of bytes than expected.\n");
         return EXIT_FAILURE;
       }
